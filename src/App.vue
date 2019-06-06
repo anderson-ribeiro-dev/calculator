b799700c3c6a6391ea77d3916cbc56d7e274c302
anderson <anderson_silvaribeiro@hotmail.com>
2019-06-06 19:42:20 -0300

    project calculator vue js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Calculadora</h1>
+        <Calculator></Calculator>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './main/Calculator'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    components: { Calculator }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@font-face {
+    font-family: "RobotoMono";
+    src: url("./fonts/RobotoMono-Thin.ttf")
 }
+
+* {
+    font-family: "RobotoMono", monospace;
+}
+
+body {
+    margin: 0;
+}
+  
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+    background: linear-gradient(to right, rgb(83, 2018, 118), rgb(41, 46, 73))
+}  
+
 </style>
---
+++ src/main/Calculator.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="calculator">
+        <Display :value="displayValue" ></Display> 
+        <Button label="AC" triple @onClick="clearMemory"></Button>  
+        <Button label="/" operation @onClick="setOperation"></Button>  
+        <Button label="7" @onClick="addDigit"></Button>  
+        <Button label="8" @onClick="addDigit"></Button>  
+        <Button label="9" @onClick="addDigit"></Button>  
+        <Button label="*" operation @onClick="setOperation"></Button>  
+        <Button label="4" @onClick="addDigit"></Button>  
+        <Button label="5" @onClick="addDigit"></Button>  
+        <Button label="6" @onClick="addDigit"></Button>  
+        <Button label="-" operation @onClick="setOperation"></Button>  
+        <Button label="1" @onClick="addDigit" ></Button>  
+        <Button label="2" @onClick="addDigit"></Button>  
+        <Button label="3" @onClick="addDigit"></Button>  
+        <Button label="+" operation @onClick="setOperation"></Button>  
+        <Button label="0" double @onClick="addDigit"></Button>  
+        <Button label="." @onClick="addDigit"></Button>  
+        <Button label="=" operation @onClick="setOperation"></Button>  
+    </div>
+</template>
+
+<script>
+import Display from '../components/Display'
+import Button from '../components/Button'
+
+export default {
+    data: function() {
+        return {
+            displayValue: "0",
+            clearDisplay: false,
+            operation: null,
+            values: [0, 0],
+            current: 0,
+        }
+    },
+    components: { Button, Display },
+    methods: {
+        clearMemory(){
+            // console.log('Limpar Memória!')
+            Object.assign(this.$data, this.$options.data()) //estado inicial do objeto
+        },
+        setOperation(operation) {
+            // console.log('Operação ' + operation )
+            if(this.current === 0) {
+                this.operation = operation
+                this.current = 1
+                this.clearDisplay = true
+            } else {
+                const equals = operation === "="
+                const currentOperation = this.operation
+
+                try {
+                    this.values[0] = eval(
+                        `${this.values[0]} ${currentOperation} ${this.values[1]}`
+                    )
+                } catch(e) {
+                    this.$emit('onError', e)
+                }
+                this.values[1] = 0
+                this.displayValue = this.values[0]
+                this.operation = equals ? null : operation
+                this.current = equals ? 0 : 1
+                this.clearDisplay = !equals
+            }
+        },
+        addDigit(n) {
+            // console.log('Digito' + n)
+            if(n === "." && this.displayValue.includes(".")) {
+                return
+            }
+
+            const clearDisplay = this.displayValue === "0"
+                || this.clearDisplay
+            const currentValue = clearDisplay ? "" : this.displayValue // valor atual
+            const displayValue = currentValue + n //novo valor
+
+            this.displayValue = displayValue //altera o estado do componente 
+            this.clearDisplay = false 
+            this.values[this.current] = displayValue 
+
+            // if (n !== ".") {
+            //     const i = this.current
+            //     const newValue = parseFloat(displayValue)
+            //     this.values[i] = newValue //indice 1 novo valor
+            // }
+        }
+    },
+}
+</script>
+
+<style>
+    .calculator {
+        height: 320px;
+        width: 235px;
+        border-radius: 5px;
+        overflow: hidden;
+
+        display: grid;
+        grid-template-columns: repeat(4, 25%);
+        grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+    }
+</style>
